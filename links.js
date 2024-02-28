a256149f029c0fe6a374fb3043c040dceea6817d
Bas Visscher <bas@koolecontrols.nl>
2024-02-28 16:30:53 +0100

    Removed links to private server

--- links.js
+++ links.js
@@ -1,31 +1,31 @@
 const jsonData = [
 
     {
-        "link": "https://portainer.vanbassum.com",
+        "link": "https://portainer.myserver.com",
         "title": "Portainer",
         "description": "Container management platform",
         "icon": "logos/portainer.svg"
     },
     {
-        "link": "https://keycloak.vanbassum.com",
+        "link": "https://keycloak.myserver.com",
         "title": "Keycloak",
         "description": "Identity and access management solution",
         "icon": "logos/keycloak.svg"
     },
     {
-        "link": "https://grafana.vanbassum.com",
+        "link": "https://grafana.myserver.com",
         "title": "Grafana",
         "description": "Open-source analytics and monitoring platform",
         "icon": "logos/grafana.svg"
     },
     {
-        "link": "https://influxdb.vanbassum.com",
+        "link": "https://influxdb.myserver.com",
         "title": "InfluxDB",
         "description": "Open-source time series database",
         "icon": "logos/influxdb.svg"
     },
     {
-        "link": "https://minio.vanbassum.com",
+        "link": "https://minio.myserver.com",
         "title": "Minio",
         "description": "High-performance, distributed object storage server",
         "icon": "logos/minio.svg"
