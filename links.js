e00513e6c9a1991ae916f899fc7ea0839806cbd3
Bas Visscher <bas@koolecontrols.nl>
2024-02-28 16:25:12 +0100

    Added project files

---
+++ links.js
@@ -0,0 +1,33 @@
+const jsonData = [
+
+    {
+        "link": "https://portainer.vanbassum.com",
+        "title": "Portainer",
+        "description": "Container management platform",
+        "icon": "logos/portainer.svg"
+    },
+    {
+        "link": "https://keycloak.vanbassum.com",
+        "title": "Keycloak",
+        "description": "Identity and access management solution",
+        "icon": "logos/keycloak.svg"
+    },
+    {
+        "link": "https://grafana.vanbassum.com",
+        "title": "Grafana",
+        "description": "Open-source analytics and monitoring platform",
+        "icon": "logos/grafana.svg"
+    },
+    {
+        "link": "https://influxdb.vanbassum.com",
+        "title": "InfluxDB",
+        "description": "Open-source time series database",
+        "icon": "logos/influxdb.svg"
+    },
+    {
+        "link": "https://minio.vanbassum.com",
+        "title": "Minio",
+        "description": "High-performance, distributed object storage server",
+        "icon": "logos/minio.svg"
+    }
+];
---
+++ script.js
@@ -0,0 +1,130 @@
+const cardContainer = document.getElementById('cardContainer');
+
+async function checkOnlineStatus(url) {
+    try {
+        const response = await fetch(url, { method: 'GET', mode: 'no-cors' });
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+function createCard(item) {
+    const card = document.createElement('div');
+    card.classList.add('col-md-6', 'col-lg-4', 'card-container');
+
+    const badgeElement = document.createElement('span');
+    badgeElement.classList.add('badge', 'badge-unknown', 'badge-secondary', 'mt-2');
+    badgeElement.textContent = 'Checking';
+
+    const cardContent = `
+        <div class="card link-card" data-url="${item.link}">
+            <div class="card-body d-flex">
+                <div class="logo-container">
+                    <img src="${item.icon}" alt="${item.title}" class="logo-img">
+                </div>
+                <div class="title-content">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-description">${item.description}</p>
+                </div>
+                <div class="buttons-container">
+                    ${createButtons(item)}
+                </div>
+            </div>
+        </div>
+    `;
+
+    card.innerHTML = cardContent;
+    card.querySelector('.title-content').appendChild(badgeElement);
+    card.addEventListener('click', handleCardClick);
+    card.addEventListener('auxclick', handleCardAuxClick);
+
+    const buttonsContainer = card.querySelector('.buttons-container');
+    if (buttonsContainer) {
+        buttonsContainer.addEventListener('click', handleButtonClicked);
+    }
+    
+    cardContainer.appendChild(card);
+    updateBadgeStatus(card, item.link);
+}
+
+function createButtons(item) {
+    if (!item.buttons) {
+        return '';
+    }
+
+    return item.buttons.map(button => `
+        <button class="btn btn-primary btn-sm" data-url="${button.url}">
+            ${button.label}
+        </button>
+    `).join('');
+}
+
+
+
+
+
+function handleCardClick(event) {
+    const clickedElement = event.target;
+    if (clickedElement.tagName !== 'BUTTON') {
+        const card = clickedElement.closest('.card');
+        if (card) {
+            const cardLink = card.getAttribute('data-url');
+            if (cardLink) {
+                window.location.href = cardLink;
+            }
+        }
+    }
+}
+
+function handleCardAuxClick(event) {
+    const clickedElement = event.target;
+    if (clickedElement.tagName !== 'BUTTON') {
+        const card = clickedElement.closest('.card');
+        if (card) {
+            const cardLink = card.getAttribute('data-url');
+            if (cardLink) {
+                if (event.button === 0) { // Left-click
+                    window.location = cardLink.href;
+                } else if (event.button === 1) { // Middle-click
+                    window.open(cardLink, '_blank');
+                }
+            }
+        }
+    }
+}
+
+function handleButtonClicked(event) {
+    const button = event.target;
+    const buttonUrl = button.getAttribute('data-url');
+    
+    if (buttonUrl) {
+        fetch(buttonUrl, { method: 'GET', mode: 'no-cors' })
+            .then(response => {
+                // Handle the response as needed
+            })
+            .catch(error => {
+                console.error('Error fetching URL:', error);
+            });
+    }
+}
+
+async function updateBadgeStatus(card, link) {
+    const badgeElement = card.querySelector('.badge-unknown');
+    try {
+        const isOnline = await checkOnlineStatus(link);
+        const badgeClass = isOnline ? 'badge-success' : 'badge-danger';
+        badgeElement.textContent = isOnline ? 'Online' : 'Offline';
+        badgeElement.classList.remove('badge-unknown');
+        badgeElement.classList.add(badgeClass);
+    } catch (error) {
+        console.error(error);
+        badgeElement.textContent = 'Unknown';
+        badgeElement.classList.remove('badge-unknown');
+        badgeElement.classList.add('badge-danger');
+    }
+}
+
+jsonData.forEach(item => {
+    createCard(item);
+});
